5f09c09f27584b81c34f3acff133acb94127d268
Lauren Miggins <lauren.miggins@gmail.com>
2020-01-30 14:40:07 -0500

    uploading files to repo

---
+++ array.js
@@ -0,0 +1,5 @@
+let addmovie = prompt("Suggest a new favorite movie to add to my top choices", "Moonstruck")
+const myArr = ['Indiana Jones and the Last Crusade', 'Bull Durham', 'The Birdcage', 'George of the Jungle', 'Grumpy Old Men']
+console.log(myArr);
+myArr.push(addmovie);
+console.log(myArr.toString()); 
